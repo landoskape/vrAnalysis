dd2c430c0e65ed9b49863a4ff29b7745dac98233
landoskape <andrewtylerlandau@gmail.com>
2023-08-09 11:22:26 +0100

    Basic functionality:
    Can now open a cursor with the context manager, retrieve information from the table and return or display it as a pandas dataframe

--- dbManagement.py
+++ dbManagement.py
@@ -1,4 +1,7 @@
 import pyodbc
+from contextlib import contextmanager
+import pandas as pd
+from pandasgui import show
 
 def vrDatabasePath(dbName):
     dbdict = {
@@ -21,14 +24,39 @@ class vrDatabase:
         )
         return pyodbc.connect(connString, readonly=self.readOnly)
     
-    def close(self,conn):
-        conn.close()
+    @contextmanager
+    def openCursor(self):
+        try:
+            conn = self.connect()
+            cursor = conn.cursor()
+            yield cursor
+        except Exception as e:
+            print(f"An exception occurred while trying to connect to {self.dbName}!")
+            raise e
+        finally:
+            cursor.close()
+            conn.close()
+    
+    def fieldNames(self):
+        with self.openCursor() as cursor:
+            fieldNames = [col.column_name for col in cursor.columns(table=self.tableName)]
+        return fieldNames
+            
+    def tableData(self):
+        with self.openCursor() as cursor:
+            cursor.execute(f"SELECT * FROM {self.tableName}")
+            tableElements = cursor.fetchall()
+        return tableElements
+    
+    def getTable(self):
+        fieldNames = self.fieldNames()
+        tableData = self.tableData()
+        return pd.DataFrame.from_records(tableData, columns=fieldNames)
         
-    def printValues(self):
-        conn = self.connect()
-        cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM {self.tableName}")
-        for row in cursor.fetchall():
-            print(row)
-        cursor.close()
-        conn.close()
+    def showTable(self):
+        show(self.getTable())
+    
+    def needsS2P(self):
+        df = self.getTable()
+        return df[(df['Imaging']==True) & (df['suite2p']==False)]
+        
