3b19d4aef4f442039261c3821c355ce2acc4c863
landoskape <andrewtylerlandau@gmail.com>
2023-08-09 15:37:41 +0100

    Fixed suite2p correction
    And am now successfully updating the database!

--- dbManagement.py
+++ dbManagement.py
@@ -13,21 +13,21 @@ def vrDatabasePath(dbName):
     
 
 class vrDatabase:
-    def __init__(self, tableName='sessiondb', dbName='vrDatabase', readOnly=True):
-        self.tableName = tableName
-        self.dbName = dbName
-        self.dbpath = vrDatabasePath(dbName)
+    def __init__(self):
+        self.tableName = 'sessiondb'
+        self.dbName = 'vrDatabase'
+        self.dbpath = vrDatabasePath(self.dbName)
         self.readOnly = readOnly
     
-    def connect(self):
+    def connect(self, readOnly=True):
         connString = (
             r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
             fr"DBQ={self.dbpath};"
         )
-        return pyodbc.connect(connString, readonly=self.readOnly)
+        return pyodbc.connect(connString, readonly=readOnly)
     
     @contextmanager
-    def openCursor(self):
+    def openCursor(self, commitChanges=False):
         try:
             conn = self.connect()
             cursor = conn.cursor()
@@ -36,9 +36,10 @@ class vrDatabase:
             print(f"An exception occurred while trying to connect to {self.dbName}!")
             raise e
         finally:
+            if commitChanges: conn.commit()
             cursor.close()
             conn.close()
-    
+            
     def fieldNames(self):
         with self.openCursor() as cursor:
             fieldNames = [col.column_name for col in cursor.columns(table=self.tableName)]
@@ -64,17 +65,36 @@ class vrDatabase:
     
     def checkS2P(self, withDatabaseUpdate=False, returnCheck=False):
         df = self.getTable()
+        
+        # return dataframe of sessions with imaging where suite2p wasn't done, even though the database thinks it was
         check_s2pDone = df[(df['Imaging']==True) & (df['suite2p']==True)]
         checked_notDone = check_s2pDone.apply(lambda row: not(self.vrSession(row).suite2pPath().exists()), axis=1)
         notActuallyDone = check_s2pDone[checked_notDone]
         
+        # return dataframe of sessions with imaging where suite2p was done, even though the database thinks it wasn't
         check_s2pNeed = df[(df['Imaging']==True) & (df['suite2p']==False)]
         checked_notNeed = check_s2pNeed.apply(lambda row: self.vrSession(row).suite2pPath().exists(), axis=1)
         notActuallyNeed = check_s2pNeed[checked_notNeed]
         
+        # Print database errors to workspace
         for idx, row in notActuallyDone.iterrows(): print(f"Database said suite2p has been ran, but it actually hasn't: {self.vrSession(row).sessionPrint()}")
         for idx, row in notActuallyNeed.iterrows(): print(f"Database said suite2p didn't run, but it already did: {self.vrSession(row).sessionPrint()}")
         
+        # If withDatabaseUpdate==True, then correct the database
+        if withDatabaseUpdate: 
+            for idx, row in notActuallyDone.iterrows():
+                cUID = row['Unique Session ID']
+                updateStatement = f"UPDATE {self.tableName} SET suite2p = False WHERE [Unique Session ID] = {cUID};"
+                with self.openCursorCommit(commitChanges=True) as cursor:
+                    cursor.execute(updateStatement)
+                    
+            for idx, row in notActuallyNeed.iterrows():
+                cUID = row['Unique Session ID']
+                updateStatement = f"UPDATE {self.tableName} SET suite2p = True WHERE [Unique Session ID] = {cUID};"
+                with self.openCursor(commitChanges=True) as cursor:
+                    cursor.execute(updateStatement)
+                    
+        # If returnCheck is requested, return True if any records were invalid
         if returnCheck: return checked_notDone.any() or checked_notNeed.any()
     
     def vrSession(self, row):
