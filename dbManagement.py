763fe23701cf8a260055bd04d0753fe5bedcee7a
landoskape <andrewtylerlandau@gmail.com>
2023-08-09 13:09:16 +0100

    Created a "checkS2P" function
    for checking whether the database has accurate suite2p information

--- dbManagement.py
+++ dbManagement.py
@@ -2,6 +2,7 @@ import pyodbc
 from contextlib import contextmanager
 import pandas as pd
 from pandasgui import show
+import vrExperiment as vre
 
 def vrDatabasePath(dbName):
     dbdict = {
@@ -10,6 +11,7 @@ def vrDatabasePath(dbName):
     if dbName not in dbdict.keys(): raise ValueError(f"Did not recognize database={dbName}, valid database names are: {[key for key in dbdict.keys()]}")
     return dbdict[dbName]
     
+
 class vrDatabase:
     def __init__(self, tableName='sessiondb', dbName='vrDatabase', readOnly=True):
         self.tableName = tableName
@@ -59,4 +61,23 @@ class vrDatabase:
     def needsS2P(self):
         df = self.getTable()
         return df[(df['Imaging']==True) & (df['suite2p']==False)]
+    
+    def checkS2P(self, withDatabaseUpdate=False, returnCheck=False):
+        df = self.getTable()
+        check_s2pDone = df[(df['Imaging']==True) & (df['suite2p']==True)]
+        checked_notDone = check_s2pDone.apply(lambda row: not(self.vrSession(row).suite2pPath().exists()), axis=1)
+        notActuallyDone = check_s2pDone[checked_notDone]
+        
+        check_s2pNeed = df[(df['Imaging']==True) & (df['suite2p']==False)]
+        checked_notNeed = check_s2pNeed.apply(lambda row: self.vrSession(row).suite2pPath().exists(), axis=1)
+        notActuallyNeed = check_s2pNeed[checked_notNeed]
+        
+        for idx, row in notActuallyDone.iterrows(): print(f"Database said suite2p has been ran, but it actually hasn't: {self.vrSession(row).sessionPrint()}")
+        for idx, row in notActuallyNeed.iterrows(): print(f"Database said suite2p didn't run, but it already did: {self.vrSession(row).sessionPrint()}")
+        
+        if returnCheck: return checked_notDone.any() or checked_notNeed.any()
+    
+    def vrSession(self, row):
+        return vre.vrSession(row['Mouse Name'], row['Session Date'].strftime('%Y-%m-%d'), str(row['Session ID']))
+        
         
