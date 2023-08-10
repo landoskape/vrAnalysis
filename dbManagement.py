9f9e94994c556c519b66403869467dd764324582
landoskape <andrewtylerlandau@gmail.com>
2023-08-10 08:24:26 +0100

    Preparing code to run vrRegistration + BUG FIX
    1. Has method for returning dataframe containing sessions that need registration
    
    2. Has method that prints sessions requiring suite2p still.
    
    3. Smarter management of converting table row into the key sessionName variables (it's in a distinct method now).
    
    4. registerSessions() method to go through all sessions that require registration and do the registration. It also updates the database to reflect that registration was performed.
    
    5. CRITICAL BUG FIX: The openCursor() function now handles errors much more intelligently. It only commits changes (when requested) if no exception was raised, and always closes the cursor and connection even if an exception was raised.

--- dbManagement.py
+++ dbManagement.py
@@ -29,14 +29,18 @@ class vrDatabase:
     @contextmanager
     def openCursor(self, commitChanges=False):
         try:
+            # Attempt to open a cursor to the database
             conn = self.connect()
             cursor = conn.cursor()
             yield cursor
-        except Exception as e:
+        except Exception as ex:
             print(f"An exception occurred while trying to connect to {self.dbName}!")
             raise e
-        finally:
+        else:
+            # if no exception was raised, commit changes
             if commitChanges: conn.commit()
+        finally:
+            # Always close the cursor and connection
             cursor.close()
             conn.close()
             
@@ -56,13 +60,22 @@ class vrDatabase:
         tableData = self.tableData()
         return pd.DataFrame.from_records(tableData, columns=fieldNames)
         
-    def showTable(self):
-        show(self.getTable())
+    def showTable(self, table=None):
+        show(self.getTable() if table is None else table)
+    
+    def needsRegistration(self): 
+        df = self.getTable()
+        return df[df['vrRegistration']==False]
     
     def needsS2P(self):
         df = self.getTable()
         return df[(df['Imaging']==True) & (df['suite2p']==False)]
     
+    def printRequiresS2P(self):
+        need = self.needsS2P()
+        for idx, row in need.iterrows():
+            print(f"Database indicates that suite2p has not been run: {self.vrSession(row).sessionPrint()}")
+
     def checkS2P(self, withDatabaseUpdate=False, returnCheck=False):
         df = self.getTable()
         
@@ -97,7 +110,70 @@ class vrDatabase:
         # If returnCheck is requested, return True if any records were invalid
         if returnCheck: return checked_notDone.any() or checked_notNeed.any()
     
+    def sessionName(self, row):
+        mouseName = row['Mouse Name']
+        sessionDate = row['Session Date'].strftime('%Y-%m-%d')
+        sessionID = str(row['Session ID'])
+        return mouseName, sessionDate, sessionID
+
     def vrSession(self, row):
-        return vre.vrSession(row['Mouse Name'], row['Session Date'].strftime('%Y-%m-%d'), str(row['Session ID']))
+        mouseName, sessionDate, sessionID = self.sessionName(row)
+        return vre.vrSession(mouseName, sessionDate, sessionID)
+    
+    def vrRegistration(self, row):
+        mouseName, sessionDate, sessionID = self.sessionName(row)
+        return vre.vrExperimentRegistration(mouseName, sessionDate, sessionID)
+    
+    def registerSession(self, **userOpts):
+        # Options for data management:
+        # These are a subset of what is available in vre.vrExperimentRegistration 
+        # They indicate what preprocessing steps to take depending on what was performed in each experiment
+        opts = {}
+        opts['vrBehaviorVersion'] = 1 # 1==standard behavioral output (will make conditional loading systems for alternative versions...)
+        opts['facecam'] = False # whether or not face video was performed on this session (note: only set to True when DLC has already been run!)
+        opts['imaging'] = True # whether or not imaging was performed on this session (note: only set to True when suite2p has already been run!)
+        opts['oasis'] = True # whether or not to rerun oasis on calcium signals (note: only used if imaging is run)
+        opts['moveRawData'] = False # whether or not to move raw data files to 'rawData'
+        opts['redCellProcessing'] = True # whether or not to preprocess redCell features into oneData using the redCellProcessing object (only runs if redcell in self.value['available'])
         
+        assert userOpts.keys() <= opts.keys(), f"userOpts contains the following invalid keys: {set(userOpts.keys()).difference(opts.keys())}"
+        opts.update(userOpts) # Update default opts with user requests
         
+        print("In registerSessions, 'vrBehaviorVersion' is an important input that hasn't been coded yet!") 
+        
+        dfToRegister = self.needsRegistration()
+        for idx, row in dfToRegister.iterrows():
+            cUID = row['Unique Session ID']
+            opts['imaging']=row['Imaging']
+            opts['facecam']=row['Face Camera']
+            vrExpReg = self.vrRegistration(row, opts)
+            try: 
+                print(f"Performing vrExperiment preprocessing for session: {vrExpReg.sessionPrint()}")
+                vrExpReg.doPreprocessing()
+                vrExpReg.saveParams()
+            except Exception as ex:
+                print(f"The following exception was raised when trying to preprocess session: {vrExpReg.sessionPrint()}")
+                print(f"Exception: {ex}")
+            else:
+                updateStatement = f"UPDATE {self.tableName} SET vrRegistration = True WHERE [Unique Session ID] = {cUID};"
+                with self.openCursor(commitChanges=True) as cursor: 
+                    cursor.execute(updateStatement)
+            finally: 
+                del vrExpReg
+                    
+            
+        
+# def checkSessionFiles(mouseName, fileIdentifier, onlyTrue=True):
+#     dataPath = fm.localDataPath()
+#     sessionPaths = glob(str(dataPath / mouseName) + '/*')
+#     for spath in sessionPaths:
+#         cdate = Path(spath).name
+#         sessions = glob(spath+'/*/')
+#         for s in sessions:
+#             cses = Path(s).name
+#             targetFiles = glob(s+fileIdentifier)
+#             numFiles = len(targetFiles)
+#             if not(onlyTrue and numFiles==0): 
+#                 print(f"{cdate}    {cses}    numFiles: {numFiles}")
+            
+# checkSessionFiles('ATL012', '*eye.mj2') #'*eye.mj2' / 'suite2p')
