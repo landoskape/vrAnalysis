a311d9553a9eb7b6bb5da06ad158d7b22f0e0db3
landoskape <andrewtylerlandau@gmail.com>
2023-09-14 14:32:36 +0100

    added scaling and errorPlot helpers

--- vrAnalysis/helpers.py
+++ vrAnalysis/helpers.py
@@ -2,7 +2,27 @@ import math
 import numpy as np
 import scipy as sp
 import matplotlib
+import matplotlib.pyplot as plt
 
+def scale(data, vmin=0, vmax=1, prctile=(0,100)):
+    '''scale data to arbitrary range using conservative percentile estimate'''
+    xmin = np.percentile(data, prctile[0])
+    xmax = np.percentile(data, prctile[1])
+    sdata = (data - xmin) / (xmax - xmin)
+    sdata *= (vmax - vmin)
+    sdata += vmin
+    return sdata
+    
+def errorPlot(x, data, axis=-1, se=False, ax=None, **kwargs):
+    '''convenience method for making a plot with errorbars
+    kwargs go into fill_between and plot, so they have to work for both'''
+    if ax is None: ax=plt.gca()
+    meanData = np.mean(data,axis=axis)
+    correction = data.shape[axis] if se else 1
+    errorData = np.std(data,axis=axis)/correction
+    ax.fill_between(x, meanData+errorData, meanData-errorData, **kwargs)
+    ax.plot(x, meanData, **kwargs)
+    
 def ncmap(name='Spectral', vmin=0., vmax=1.):
     cmap = matplotlib.cm.get_cmap(name)
     norm = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
