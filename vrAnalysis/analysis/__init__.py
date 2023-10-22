8ed5d96d50ef501a9c18eb087f77cf6872a8c5c9
Andrew Landau <andrewtylerlandau@gmail.com>
2023-10-22 10:00:55 +0100

    Update __init__.py

--- vrAnalysis/analysis/__init__.py
+++ vrAnalysis/analysis/__init__.py
@@ -2,4 +2,4 @@ from .sameCellCandidates import sameCellCandidates, clusterExplorer
 from .piezoConsistency import piezoConsistency
 from .placeCellSingleSession import placeCellSingleSession, red_reliability, plot_reliable_difference, plot_reliable_fraction
 from .placeCellMultiSession import placeCellMultiSession
-
+from .sessionStats import sessionStats, plot_environmentStats, plot_roiCountStats
