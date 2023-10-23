4418f3763987a5d1213c5ec029f6f5c79278624a
Andrew Landau <andrewtylerlandau@gmail.com>
2023-10-23 14:31:56 +0100

    bye

--- usefulCodeToRemember.py
+++
@@ -1,5 +0,0 @@
-# from PyQt5 import QtCore, QtWidgets
-# modifiers = QtWidgets.QApplication.keyboardModifiers()
-#if modifiers==QtCore.Qt.ShiftModifier: print('helloshift')
-#if modifiers==QtCore.Qt.ControlModifier: print('hellocontrol')
-#if modifiers == (QtCore.Qt.ControlModifier | QtCore.Qt.ShiftModifier): print('helloshiftcontrol')
