806940ab23423d901afb78e34354279bbaf43cf9
Andrew Landau <andrewtylerlandau@gmail.com>
2024-01-18 08:19:29 +0000

    integrate "True" tracked pairs binary matrix into workflows

--- vrAnalysis/analysis/roicat_stats.py
+++ vrAnalysis/analysis/roicat_stats.py
@@ -44,13 +44,15 @@ class RoicatStats(placeCellMultiSession):
         pass
 
     @handle_idx_ses
-    def make_roicat_comparison(self, envnum, sim_name='sConj', tracked=False, idx_ses=None, cutoffs=(0.4, 0.7), both_reliable=False):        
+    def make_roicat_comparison(self, envnum, sim_name='sConj', idx_ses=None, cutoffs=(0.4, 0.7), both_reliable=False):        
         """
         load ROICaT comparison data
         
-        returns sim, corr, and prms, where sim and corr are lists of values associated with each pair of ROIs in a pair of sessions
+        returns sim, corr, tracked, and prms, where:
+        sim, corr, and tracked are lists of values associated with each pair of ROIs in a pair of sessions
         sim contains the ROICaT similarity value named by **sim_name**
         corr contains the place field correlation value
+        tracked contains a 1 if the pair is tracked by ROICaT and a zero otherwise
 
         prms is a dictionary describing the data parameters (which sessions, which session pairs are used, which environment)
 
@@ -60,32 +62,42 @@ class RoicatStats(placeCellMultiSession):
         idx_ses_pairs = helpers.all_pairs(idx_ses)
 
         # get all spkmaps from requested sessions
-        spkmaps, relmse, relcor, pfloc, _, _ = self.get_spkmaps(envnum, trials='full', average=True, tracked=tracked, idx_ses=idx_ses, by_plane=True)
+        spkmaps, relmse, relcor, pfloc, _, _ = self.get_spkmaps(envnum, trials='full', average=True, tracked=False, idx_ses=idx_ses, by_plane=True)
 
         # define reliability metric
         idx_reliable = [[(mse>cutoffs[0]) & (cor>cutoffs[1]) for mse, cor in zip(rmse, rcor)] for rmse, rcor in zip(relmse, relcor)]
 
+        # get all labels from requested sessions
+        tracked_labels = self.track.get_tracked_labels(cat_planes=False, nan_untracked=True, idx_ses=idx_ses, keep_planes=self.keep_planes) 
+
         # for each source/target pair in idx_ses, do: 
-        sim, corr = [], []       
+        sim, corr, tracked = [], [], []    
         for source, target in idx_ses_pairs:
             isource, itarget = helpers.index_in_target(source, idx_ses)[1][0], helpers.index_in_target(target, idx_ses)[1][0]
 
             # get similarity data from source/target
-            sim_paired = self.track.get_similarity_paired(sim_name, source=source, target=target, symmetric=True, tracked=tracked, cat_planes=False, keep_planes=self.keep_planes)
+            sim_paired = self.track.get_similarity_paired(sim_name, source=source, target=target, symmetric=True, tracked=False, cat_planes=False, keep_planes=self.keep_planes)
             
             # compute correlation between source and target
             corrs = [helpers.crossCorrelation(spksource.T, spktarget.T) for spksource, spktarget in zip(spkmaps[isource], spkmaps[itarget])]
             
+            # retrieve source/target labels for each ROI by plane
+            tracked_pair = [label_source.reshape(-1, 1) == label_target.reshape(1, -1) for label_source, label_target in zip(tracked_labels[isource], tracked_labels[itarget])]
+
             # filter by reliability
             sim_paired = [sim[idx_source] for sim, idx_source in zip(sim_paired, idx_reliable[isource])]
             corrs = [cor[idx_source] for cor, idx_source in zip(corrs, idx_reliable[isource])]
+            tracked_pair = [pair[idx_source] for pair, idx_source in zip(tracked_pair, idx_reliable[isource])]
             if both_reliable:
                 sim_paired = [sim[:, idx_target] for sim, idx_target in zip(sim_paired, idx_reliable[itarget])]
                 corrs = [cor[:, idx_target] for cor, idx_target in zip(corrs, idx_reliable[itarget])]
+                tracked_pair = [pair[idx_target] for pair, idx_target in zip(tracked_pair, idx_reliable[itarget])]
 
+            # makes 1s for tracked pairs
             # stack and flatten across planes
             sim.append(np.concatenate([s.toarray().flatten() for s in sim_paired]))
             corr.append(np.concatenate([c.flatten() for c in corrs]))
+            tracked.append(np.concatenate([p.flatten() for p in tracked_pair]))
 
         # result parameters
         prms = dict(
@@ -93,23 +105,34 @@ class RoicatStats(placeCellMultiSession):
             idx_ses=idx_ses,
             idx_ses_pairs=idx_ses_pairs,
             sim_name=sim_name,
+            cutoffs=cutoffs, 
+            both_reliable=both_reliable,
         )
 
-        return sim, corr, prms
+        return sim, corr, tracked, prms
 
-    def split_by_roicat_assignment(self, sim, corr):
-        """split corr values by roicat assignment (comparing sim to cutoffs)"""
-        idx_roicat_diff = [s < self.sim_cutoffs[0] for s in sim]
-        idx_roicat_same = [s > self.sim_cutoffs[1] for s in sim]
+    def split_by_roicat_assignment(self, sim, corr, tracked=None):
+        """
+        split corr values by roicat assignment
+        
+        either compare sim to cutoffs set as default attribute of self
+        or use tracked pair identities in tracked
+        """
+        if tracked is not None:
+            idx_roicat_same = [t==True for t in tracked]
+            idx_roicat_diff = [t==False for t in tracked]
+        else:
+            idx_roicat_diff = [s < self.sim_cutoffs[0] for s in sim]
+            idx_roicat_same = [s > self.sim_cutoffs[1] for s in sim]
         corr_diff = [c[idx] for c, idx in zip(corr, idx_roicat_diff)]
         corr_same = [c[idx] for c, idx in zip(corr, idx_roicat_same)]
         return corr_same, corr_diff
 
-    def plot_sim_vs_pfcorr(self, sim, corr, prms, color_by_density=False, with_show=True, with_save=False):
+    def plot_sim_vs_pfcorr(self, sim, corr, tracked, prms, color_by_density=False, with_show=True, with_save=False):
         """
         helper for making scatter plots between sim and corr
 
-        inputs are sim, corr, and prms which come from make_roicat_comparison()
+        inputs are sim, corr, tracked, and prms which come from make_roicat_comparison()
 
         if color_by_density=True, will color plots by their local density with gaussian_kde
         (keep in mind that this is slow)
@@ -146,7 +169,7 @@ class RoicatStats(placeCellMultiSession):
         # Show figure if requested
         plt.show() if with_show else plt.close()
 
-    def plot_pfcorr_by_samediff(self, sim, corr, prms, with_show=True, with_save=False):
+    def plot_pfcorr_by_samediff(self, sim, corr, tracked, prms, with_show=True, with_save=False):
         """
         helper for plotting pfcorr values for same and different populations
         
@@ -156,7 +179,7 @@ class RoicatStats(placeCellMultiSession):
 
         inputs are sim, corr, and prms which come from make_roicat_comparison()
         """
-        corr_same, corr_diff = self.split_by_roicat_assignment(sim, corr)
+        corr_same, corr_diff = self.split_by_roicat_assignment(sim, corr, tracked=tracked)
         
         dataframes = []
         for name, csame, cdiff in zip(self.session_pair_names(prms), corr_same, corr_diff):
