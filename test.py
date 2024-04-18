73ae834434e9032174da7394e56ca5eb7b4304e6
Andrew Landau <andrewtylerlandau@gmail.com>
2024-04-18 16:05:54 +0100

    run tests

--- test.py
+++ test.py
@@ -102,22 +102,22 @@ if __name__ == "__main__":
     for mouse_name in mouse_names:
         print(f"Analyzing {mouse_name}")
 
-        # load spectra data for target mouse
-        track = tracking.tracker(mouse_name)  # get tracker object for mouse
-        pcm = analysis.placeCellMultiSession(track, autoload=False)  # open up place cell multi session analysis object (don't autoload!!!)
+        # # load spectra data for target mouse
+        # track = tracking.tracker(mouse_name)  # get tracker object for mouse
+        # pcm = analysis.placeCellMultiSession(track, autoload=False)  # open up place cell multi session analysis object (don't autoload!!!)
 
-        # load spectra data (use temp if it matches)
-        args = helpers.AttributeDict(
-            dict(
-                mouse_name=mouse_name,
-                dist_step=1,
-                smooth=0.1,
-                cutoffs=(0.4, 0.7),
-                maxcutoffs=None,
-                reload_spectra_data=False,
-            )
-        )
+        # # load spectra data (use temp if it matches)
+        # args = helpers.AttributeDict(
+        #     dict(
+        #         mouse_name=mouse_name,
+        #         dist_step=1,
+        #         smooth=0.1,
+        #         cutoffs=(0.4, 0.7),
+        #         maxcutoffs=None,
+        #         reload_spectra_data=False,
+        #     )
+        # )
 
-        add_to_spectra_data(pcm, args)
+        # add_to_spectra_data(pcm, args)
 
-        # os.system(f"python scripts/mouse_summary.py --mouse-name {mouse_name} --do-spectra")
+        os.system(f"python scripts/mouse_summary.py --mouse-name {mouse_name} --do-spectra")
