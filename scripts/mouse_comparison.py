07b7ea6c3dc584c6d195a8f36566250a8bb7cd06
Andrew Landau <andrewtylerlandau@gmail.com>
2024-05-21 10:50:41 +0100

    change plots

--- scripts/mouse_comparison.py
+++ scripts/mouse_comparison.py
@@ -80,6 +80,7 @@ def get_spectra(mouse_name, args):
 
 
 def make_comparison_plots(pcms, spectra_data):
+    pass
     # ylog_min = 1e-3
     # single_env, across_env = compare_spectral_averages(spectra_data)
     # for do_xlog in [True, False]:
@@ -87,8 +88,8 @@ def make_comparison_plots(pcms, spectra_data):
     #         plot_spectral_averages_comparison(
     #             pcms, single_env, across_env, do_xlog=do_xlog, do_ylog=do_ylog, ylog_min=ylog_min, with_show=False, with_save=True
     #         )
-    for include_cvpca in [True, False]:
-        plot_svca_vs_cvpca(pcms, spectra_data, include_cvpca=include_cvpca, do_ylog=True, with_show=False, with_save=True)
+    # for include_cvpca in [True, False]:
+    #     plot_svca_vs_cvpca(pcms, spectra_data, include_cvpca=include_cvpca, do_ylog=True, with_show=False, with_save=True)
     # for relative_session in [True, False]:
     #     plot_all_exponential_fits(pcms, spectra_data, relative_session=relative_session, with_show=False, with_save=True)
     #     plot_total_variance_comparison(pcms, spectra_data, relative_session=relative_session, with_show=False, with_save=True)
