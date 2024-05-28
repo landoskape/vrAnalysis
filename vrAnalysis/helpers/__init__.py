c2726c321d69a7c20d346393c0b94c6a2aeb3025
Andrew Landau <andrewtylerlandau@gmail.com>
2024-05-28 14:32:53 +0100

    Update __init__.py

--- vrAnalysis/helpers/__init__.py
+++ vrAnalysis/helpers/__init__.py
@@ -2,7 +2,7 @@ from .indexing import *
 from .interactive_plots import *
 from .linear_algebra import *
 from .plotting import *
-from .wrangling import *
 from .signals import *
 from .typing import *
 from .vrsupport import *
+from .wrangling import *
