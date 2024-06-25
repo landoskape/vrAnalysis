a86dbb4ba0741c6b5c2474817aa115cfbd60c3fe
Andrew Landau <andrewtylerlandau@gmail.com>
2024-06-25 16:13:23 +0100

    imports

--- dimilibi/__init__.py
+++ dimilibi/__init__.py
@@ -3,5 +3,5 @@ from .networks import SVCANet
 from .pca import PCA
 from .population import Population
 from .regression import ReducedRankRegression
-from .regularizer import LocalSmoothness
+from .regularizer import LocalSmoothness, LocalSimilarity, FlexibleFilter
 from .svca import SVCA
