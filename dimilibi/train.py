287a075de5f4d6aa06c1fc406823b0d63fde5ec4
Andrew Landau <andrewtylerlandau@gmail.com>
2024-06-28 15:06:32 +0100

    train boilerplate code

--- dimilibi/train.py
+++ dimilibi/train.py
@@ -7,8 +7,6 @@ import os
 mainPath = os.path.dirname(os.path.abspath(__file__)) + "/.."
 sys.path.append(mainPath)
 
-from vrAnalysis.helpers import named_transpose
-
 from .metrics import scaled_mse
 
 
@@ -23,7 +21,8 @@ def train(
     train_target,
     val_source,
     val_target,
-    num_timepoints,
+    test_source,
+    test_target,
     batch_size,
     betavae=False,
     device="cuda",
@@ -37,22 +36,27 @@ def train(
     train_reg = torch.zeros((num_nets, num_epochs))
     train_betareg = torch.zeros((num_nets, num_epochs))
     train_scaled_mse = torch.zeros((num_nets, num_epochs))
-    trainval_loss = torch.zeros((num_nets, num_epochs))
-    trainval_score = torch.zeros((num_nets, num_epochs))
-    trainval_scaled_mse = torch.zeros((num_nets, num_epochs))
+    val_loss = torch.zeros((num_nets, num_epochs))
+    val_score = torch.zeros((num_nets, num_epochs))
+    val_scaled_mse = torch.zeros((num_nets, num_epochs))
+    test_loss = torch.zeros((num_nets, num_epochs))
+    test_score = torch.zeros((num_nets, num_epochs))
+    test_scaled_mse = torch.zeros((num_nets, num_epochs))
 
     train_source = train_source.to(device)
     train_target = train_target.to(device)
     val_source = val_source.to(device)
     val_target = val_target.to(device)
+    test_source = test_source.to(device)
+    test_target = test_target.to(device)
 
     for net in nets:
         net.train()
 
-    progress = tqdm(range(num_epochs), desc="Training SVCA Net")
+    progress = tqdm(range(num_epochs), desc="Training Networks")
     for epoch in progress:
 
-        itime = torch.randperm(num_timepoints)[:batch_size]
+        itime = torch.randperm(train_source.size(1))[:batch_size]
 
         source_batch = train_source[:, itime].T
         target_batch = train_target[:, itime].T
@@ -65,7 +69,7 @@ def train(
             mulogvar = [pred[1:] for pred in predictions]
             predictions = [pred[0] for pred in predictions]
         else:
-            mulogvar = [torch.tensor(0.0, device=device) for _ in range(num_nets)]
+            mulogvar = [(torch.tensor(0.0, device=device), torch.tensor(0.0, device=device)) for _ in range(num_nets)]
 
         losses = [loss_function(pred, target_batch) for pred in predictions]
         standard_regs = [reg(source_batch, pred) for reg, pred in zip(regularizers, predictions)]
@@ -85,17 +89,28 @@ def train(
             train_reg[inet, epoch] = standard_regs[inet].item()
             train_betareg[inet, epoch] = beta_regs[inet].item()
             train_score[inet, epoch] = scores[inet].item()
-            train_scaled_mse[inet, epoch] = scaled_mse(predictions[inet], target_batch).item()
+            train_scaled_mse[inet, epoch] = scaled_mse(predictions[inet].T, target_batch.T).item()
 
             with torch.no_grad():
                 for net in nets:
                     net.eval()
+
+                # evaluate networks on validation set
                 pred = nets[inet](val_source.T)
                 if betavae:
                     pred = pred[0]
-                trainval_loss[inet, epoch] = loss_function(pred, val_target.T).item()
-                trainval_score[inet, epoch] = nets[inet].score(val_source.T, val_target.T).item()
-                trainval_scaled_mse[inet, epoch] = scaled_mse(pred, val_target.T).item()
+                val_loss[inet, epoch] = loss_function(pred, val_target.T).item()
+                val_score[inet, epoch] = nets[inet].score(val_source.T, val_target.T).item()
+                val_scaled_mse[inet, epoch] = scaled_mse(pred.T, val_target).item()
+
+                # evaluate networks on test set
+                pred = nets[inet](test_source.T)
+                if betavae:
+                    pred = pred[0]
+                test_loss[inet, epoch] = loss_function(pred, test_target.T).item()
+                test_score[inet, epoch] = nets[inet].score(test_source.T, test_target.T).item()
+                test_scaled_mse[inet, epoch] = scaled_mse(pred.T, test_target).item()
+
                 for net in nets:
                     net.train()
 
@@ -105,7 +120,12 @@ def train(
         train_reg=train_reg,
         train_betareg=train_betareg,
         train_scaled_mse=train_scaled_mse,
-        trainval_loss=trainval_loss,
-        trainval_score=trainval_score,
-        trainval_scaled_mse=trainval_scaled_mse,
+        val_loss=val_loss,
+        val_score=val_score,
+        val_scaled_mse=val_scaled_mse,
+        test_loss=test_loss,
+        test_score=test_score,
+        test_scaled_mse=test_scaled_mse,
     )
+
+    return results
