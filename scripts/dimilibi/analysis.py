c256dc96fc6cb377d7c1f24e0ca7b309e2bb414e
Andrew Landau <andrewtylerlandau@gmail.com>
2024-07-01 17:21:37 +0100

    bugs

--- scripts/dimilibi/analysis.py
+++ scripts/dimilibi/analysis.py
@@ -3,14 +3,14 @@ from argparse import ArgumentParser
 from helpers import get_sessions, make_and_save_populations
 from rrr_optimization import do_rrr_optimization
 
-# from .network_optimization import do_network_optimization
+from network_optimization import do_network_optimization
 
 
 def parse_args():
     parser = ArgumentParser(description="Run analysis on all sessions.")
     parser.add_argument("--redo_pop_splits", default=False, action="store_true", help="Remake population objects and train/val/test splits.")
     parser.add_argument("--rrr", default=False, action="store_true", help="Run reduced rank regression optimization.")
-    parser.add_argument("--network", default=False, action="store_true", help="Run network optimization.")
+    parser.add_argument("--networks", default=False, action="store_true", help="Run network optimization.")
     return parser.parse_args()
 
 
@@ -28,6 +28,5 @@ if __name__ == "__main__":
     if args.rrr:
         do_rrr_optimization(all_sessions)
 
-    if args.network:
-        pass
-        # do_network_optimization(all_sessions)
+    if args.networks:
+        do_network_optimization(all_sessions)
