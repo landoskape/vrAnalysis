3ea83d8cb3726b099e2bc39ae635d6a9eedd8453
Andrew Landau <andrewtylerlandau@gmail.com>
2025-02-27 14:05:51 +0000

    small changes to verbosity and other things

--- scripts/add_spkmap_processor_cache.py
+++ scripts/add_spkmap_processor_cache.py
@@ -2,43 +2,34 @@ import shutil
 from tqdm import tqdm
 import numpy as np
 from vrAnalysis import database
-from vrAnalysis2.processors.spkmaps import SpkmapProcessor, SpkmapParams
+from vrAnalysis2.processors.spkmaps import SpkmapProcessor, get_spkmap_params
 from vrAnalysis2.sessions import create_b2session
 
 # Go through all the sessions with imaging
 sessiondb = database.vrDatabase("vrSessions")
 ises = sessiondb.iterSessions(imaging=True)
 
-# Default Params
-default_spkmap_params = SpkmapParams(
-    dist_step=1.0,
-    speed_threshold=1.0,
-    speed_max_allowed=np.inf,
-    full_trial_flexibility=3.0,
-    standardize_spks=True,
-    smooth_width=1.0,
-    autosave=True,
-)
 
-
-def cache_processed_maps(ises, spks_type, max_gb=1000):
+def cache_processed_maps(ises, spks_type, params_type="default", max_gb=1000):
+    params = get_spkmap_params(params_type, updates={"autosave": True})
     total_gb = 0
-    for ses in tqdm(ises, desc="Processing sessions...", leave=True):
-        session = create_b2session(ses.mouseName, ses.dateString, ses.sessionid, spks_type)
-        spkmap_processor = SpkmapProcessor(session, params=default_spkmap_params)
+    progress = tqdm(ises, desc="Processing sessions...", leave=True)
+    for ses in progress:
+        session = create_b2session(ses.mouseName, ses.dateString, ses.sessionid, dict(spks_type=spks_type))
+        spkmap_processor = SpkmapProcessor(session, params=params)
         # Use this to get the maps (which will save them if they don't exist with the current params)
         maps = spkmap_processor.maps(clear_one_cache=True)
         total_gb += maps.nbytes() / 1024**3
-        print("Finished with session: ", session, "Current GB: ", total_gb)
+        progress.set_postfix(total_gb=total_gb)
         if total_gb > max_gb:
-            print("Total GB: ", total_gb)
+            print("Reached max GB, stopping.")
             break
 
 
 def clear_cache(ises):
     for ses in tqdm(ises, desc="Clearing cache...", leave=True):
         session = create_b2session(ses.mouseName, ses.dateString, ses.sessionid)
-        spkmap_processor = SpkmapProcessor(session, params=default_spkmap_params)
+        spkmap_processor = SpkmapProcessor(session)
         cache_dir = spkmap_processor.cache_directory()
         if cache_dir.exists():
             print("Removing: ", cache_dir)
@@ -48,4 +39,5 @@ def clear_cache(ises):
 
 if __name__ == "__main__":
     # # # # # clear_cache(ises)
-    cache_processed_maps(ises, "significant")
+    cache_processed_maps(ises, "significant", params_type="default")
+    cache_processed_maps(ises, "significant", params_type="smoothed")
