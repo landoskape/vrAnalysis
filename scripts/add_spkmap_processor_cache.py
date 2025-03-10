a53de2b482a08679f7148775934ea653718d95cb
Andrew Landau <andrewtylerlandau@gmail.com>
2025-03-10 13:30:41 +0000

    add cache for reliability, change kwargs a bit

--- scripts/add_spkmap_processor_cache.py
+++ scripts/add_spkmap_processor_cache.py
@@ -1,24 +1,23 @@
 import shutil
 from tqdm import tqdm
 import numpy as np
-from vrAnalysis import database
+from vrAnalysis2.database import get_database
 from vrAnalysis2.processors.spkmaps import SpkmapProcessor, get_spkmap_params
 from vrAnalysis2.sessions import create_b2session
 
 # Go through all the sessions with imaging
-sessiondb = database.vrDatabase("vrSessions")
-ises = sessiondb.iterSessions(imaging=True)
+sessiondb = get_database("vrSessions")
+ises = sessiondb.iter_sessions(imaging=True)
 
 
-def cache_processed_maps(ises, spks_type, params_type="default", max_gb=1000):
+def cache_processed_maps(ises, params_type="default", force_recompute: bool = False, max_gb=1000):
     params = get_spkmap_params(params_type, updates={"autosave": True})
     total_gb = 0
     progress = tqdm(ises, desc="Processing sessions...", leave=True)
-    for ses in progress:
-        session = create_b2session(ses.mouseName, ses.dateString, ses.sessionid, dict(spks_type=spks_type))
+    for session in progress:
         spkmap_processor = SpkmapProcessor(session, params=params)
         # Use this to get the maps (which will save them if they don't exist with the current params)
-        maps = spkmap_processor.maps(clear_one_cache=True)
+        maps = spkmap_processor.get_processed_maps(clear_one_cache=True, force_recompute=force_recompute)
         total_gb += maps.nbytes() / 1024**3
         progress.set_postfix(total_gb=total_gb)
         if total_gb > max_gb:
@@ -26,9 +25,32 @@ def cache_processed_maps(ises, spks_type, params_type="default", max_gb=1000):
             break
 
 
+def cache_env_maps(ises, params_type="default", force_recompute: bool = False, max_gb=1000):
+    params = get_spkmap_params(params_type, updates={"autosave": True})
+    total_gb = 0
+    progress = tqdm(ises, desc="Processing sessions...", leave=True)
+    for session in progress:
+        spkmap_processor = SpkmapProcessor(session, params=params)
+        # Use this to get the maps (which will save them if they don't exist with the current params)
+        maps = spkmap_processor.get_env_maps(use_session_filters=False, force_recompute=force_recompute)
+        total_gb += maps.nbytes() / 1024**3
+        progress.set_postfix(total_gb=total_gb)
+        if total_gb > max_gb:
+            print("Reached max GB, stopping.")
+            break
+
+
+def cache_reliability(ises, params_type="default", force_recompute: bool = False):
+    params = get_spkmap_params(params_type, updates={"autosave": True})
+    progress = tqdm(ises, desc="Processing sessions...", leave=True)
+    for session in progress:
+        spkmap_processor = SpkmapProcessor(session, params=params)
+        # Use this to get the maps (which will save them if they don't exist with the current params)
+        _ = spkmap_processor.get_reliability(use_session_filters=False, force_recompute=force_recompute)
+
+
 def clear_cache(ises):
-    for ses in tqdm(ises, desc="Clearing cache...", leave=True):
-        session = create_b2session(ses.mouseName, ses.dateString, ses.sessionid)
+    for session in tqdm(ises, desc="Clearing cache...", leave=True):
         spkmap_processor = SpkmapProcessor(session)
         cache_dir = spkmap_processor.cache_directory()
         if cache_dir.exists():
@@ -39,5 +61,10 @@ def clear_cache(ises):
 
 if __name__ == "__main__":
     # # # # # clear_cache(ises)
-    cache_processed_maps(ises, "significant", params_type="default")
-    cache_processed_maps(ises, "significant", params_type="smoothed")
+    force_recompute = True
+    cache_processed_maps(ises, params_type="default", force_recompute=force_recompute)
+    cache_processed_maps(ises, params_type="smoothed", force_recompute=force_recompute)
+    cache_env_maps(ises, params_type="default", force_recompute=force_recompute)
+    cache_env_maps(ises, params_type="smoothed", force_recompute=force_recompute)
+    cache_reliability(ises, params_type="default", force_recompute=force_recompute)
+    cache_reliability(ises, params_type="smoothed", force_recompute=force_recompute)
