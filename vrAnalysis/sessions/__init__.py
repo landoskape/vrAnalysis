d5b769c55662d7269f61607a3c8cdde5dd440c68
Andrew Landau <andrewtylerlandau@gmail.com>
2025-11-13 20:01:54 +0000

    typing

--- vrAnalysis/sessions/__init__.py
+++ vrAnalysis/sessions/__init__.py
@@ -1,3 +1,3 @@
-from .b2session import B2Session, B2SessionParams
+from .b2session import B2Session, B2SessionParams, SpksTypes
 
-__all__ = ["B2Session", "B2SessionParams"]
+__all__ = ["B2Session", "B2SessionParams", "SpksTypes"]
