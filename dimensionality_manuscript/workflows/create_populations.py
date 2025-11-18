36286947379e133669b485e6593c8816bb164a2d
Andrew Landau <andrewtylerlandau@gmail.com>
2025-11-18 16:34:37 +0000

    basic workflow improvement / extensions

--- dimensionality_manuscript/workflows/create_populations.py
+++ dimensionality_manuscript/workflows/create_populations.py
@@ -4,8 +4,10 @@ from vrAnalysis.database import get_database
 from dimensionality_manuscript.regression_models.registry import PopulationRegistry
 
 clear_population = False  # Clears population cache
-check_exists = True  # Makes only if not existing
+check_exists = False  # Makes only if not existing
 force_remake = False  # Remakes even if existing
+
+show_not_existing = True  # Prints sessions that don't exist in the registry
 
 if __name__ == "__main__":
     sessiondb = get_database("vrSessions")
@@ -18,6 +20,14 @@ if __name__ == "__main__":
         if (check_exists and not registry._check_population_exists(ses)) or force_remake:
             # If either checking existence and doesn't yet exist, or forcing to remake, ...
             # ... make the population potentially with a force remake
-            _ = registry.get_population(ses, force_remake=force_remake)
+            try:
+                _ = registry.get_population(ses, force_remake=force_remake)
+            except Exception as e:
+                print(f"Error creating population for session {ses.session_print()}: {e}")
+                continue
+            finally:
+                registry.clear_population_cache()
+                ses.clear_cache()
 
-        ses.clear_cache()
+        if show_not_existing and not registry._check_population_exists(ses):
+            print(f"!!!!! Session {ses.session_print()} does not exist in the registry")
--- dimensionality_manuscript/workflows/score_models.py
+++ dimensionality_manuscript/workflows/score_models.py
@@ -2,6 +2,7 @@ import gc
 from tqdm import tqdm
 import torch
 from vrAnalysis.database import get_database
+from vrAnalysis.sessions import SpksTypes
 from dimensionality_manuscript.regression_models.registry import ModelName, PopulationRegistry, get_model
 
 clear_hyperparameters = False  # Clears hyperparameter cache
@@ -19,9 +20,17 @@ check_existing_scores = False  # Checks if scores already exist
 MODEL_NAMES: list[ModelName] = [
     "external_placefield_1d",
     "internal_placefield_1d",
-    # "external_placefield_1d_gain",
-    # "internal_placefield_1d_gain",
+    "external_placefield_1d_gain",
+    "internal_placefield_1d_gain",
+    # "rbfpos",
+    # "rrr",
 ]
+
+SPKS_TYPES: tuple[SpksTypes] = (
+    "oasis",
+    # "sigrebase",
+    # "deconvolved",
+)
 
 if __name__ == "__main__":
     sessiondb = get_database("vrSessions")
@@ -30,31 +39,35 @@ if __name__ == "__main__":
     for model_name in tqdm(MODEL_NAMES, desc="Testing different model types"):
         model = get_model(model_name, registry)
 
-        spks_type = "significant"
-        for session in tqdm(sessiondb.iter_sessions(imaging=True, session_params=dict(spks_type=spks_type))):
-            if clear_hyperparameters:
-                model.clear_cached_hyperparameter(session)
+        for spks_type in SPKS_TYPES:
+            for session in tqdm(sessiondb.iter_sessions(imaging=True, session_params=dict(spks_type=spks_type))):
+                if clear_hyperparameters:
+                    model.clear_cached_hyperparameter(session, spks_type=spks_type)
 
-            if clear_scores:
-                model.clear_cached_score(session)
+                if clear_scores:
+                    model.clear_cached_score(session, spks_type=spks_type)
 
-            if score_models:
-                try:
-                    _ = model.get_best_score(session, force_remake=force_remake, force_reoptimize=force_reoptimize)
-                except Exception as e:
-                    error_path = registry.registry_paths.error_path / f"{model_name}_{session.session_print(joinby='.')}.txt"
-                    with open(error_path, "w") as f:
-                        f.write(str(e))
-                    print(f"Error scoring model {model_name} on session {session.session_print()}: {e}")
-                    continue
+                if score_models:
+                    try:
+                        _ = model.get_best_score(session, spks_type=spks_type, force_remake=force_remake, force_reoptimize=force_reoptimize)
 
-            if check_existing_scores:
-                if model.check_existing_score(session):
-                    print(f"Score for model {model_name} on session {session.session_print()} already exists")
-                else:
-                    print(f"!!!!! Score for model {model_name} on session {session.session_print()} does not exist")
+                    except Exception as e:
+                        error_path = registry.registry_paths.error_path / f"{model_name}_{session.session_print(joinby='.')}.txt"
+                        with open(error_path, "w") as f:
+                            f.write(str(e))
+                        print(f"Error scoring model {model_name} on session {session.session_print()}: {e}")
+                        continue
 
-            # Make sure any stored data is cleared (not actually sure if torch is saving things but better clear in case)
-            session.clear_cache()
-            torch.cuda.empty_cache()
-            gc.collect()
+                    finally:
+                        # Make sure any stored data is cleared (not actually sure if torch is saving things but better clear in case)
+                        session.clear_cache()
+                        torch.cuda.empty_cache()
+                        gc.collect()
+
+                if check_existing_scores:
+                    if model.check_existing_score(session, spks_type=spks_type):
+                        print(f"Score for model {model_name} on session {session.session_print()} already exists")
+                        pass
+                    else:
+                        print(f"!!!!! Score for model {model_name} on session {session.session_print()} does not exist")
+                        pass
