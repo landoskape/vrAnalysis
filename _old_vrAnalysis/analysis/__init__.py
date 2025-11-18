74180642e4402f128f740b6b2e18db75ecbe4859
Andrew Landau <andrewtylerlandau@gmail.com>
2025-11-18 16:31:42 +0000

    fix imports

--- _old_vrAnalysis/analysis/__init__.py
+++ _old_vrAnalysis/analysis/__init__.py
@@ -1,4 +1,4 @@
-from .sameCellCandidates import sameCellCandidates, clusterExplorer, clusterExplorerROICaT
+from ._nowhere_sameCellCandidates import sameCellCandidates, clusterExplorer, clusterExplorerROICaT
 from .piezoConsistency import piezoConsistency
 from .placeCellSingleSession import (
     placeCellSingleSession,
@@ -6,8 +6,8 @@ from .placeCellSingleSession import (
     plot_reliable_difference,
     plot_reliable_fraction,
 )
-from .spikemap_methods import SpikemapMethods
+from ._nowhere_spikemap_methods import SpikemapMethods
 from .placeCellMultiSession import placeCellMultiSession
-from .roicat_stats import RoicatStats
-from .sessionStats import sessionStats, plot_environmentStats, plot_roiCountStats
+from ._nowhere_roicat_stats import RoicatStats
+from ._nowhere_sessionStats import sessionStats, plot_environmentStats, plot_roiCountStats
 from .variance_structure import VarianceStructure
