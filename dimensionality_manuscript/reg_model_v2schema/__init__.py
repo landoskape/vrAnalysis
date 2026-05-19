7fb84b74fe7ad834c5763444d6835bc8c3157627
Andrew Landau <andrewtylerlandau@gmail.com>
2026-05-19 12:43:55 +0100

    regression model schematics

---
+++ dimensionality_manuscript/reg_model_v2schema/__init__.py
@@ -0,0 +1,12 @@
+from .schematic import (
+    ModelLine,
+    ModelName,
+    MODEL_NAMES,
+    FEATURE_COLUMNS,
+    DEFAULT_COLOR_LOOKUP,
+    get_model_config,
+    plot_schematic,
+    get_coords,
+    get_model_names,
+    get_model_col_groups,
+)
