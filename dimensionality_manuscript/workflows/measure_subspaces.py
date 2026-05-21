b87cabdc518f9c93cc31a15199c3141b314935d6
Andrew Landau <andrewtylerlandau@gmail.com>
2026-05-21 09:03:45 +0100

    Update measure_subspaces.py

--- dimensionality_manuscript/workflows/measure_subspaces.py
+++ dimensionality_manuscript/workflows/measure_subspaces.py
@@ -3,26 +3,13 @@ from tqdm import tqdm
 import torch
 from vrAnalysis.database import get_database
 from vrAnalysis.sessions import SpksTypes
-from ..registry import SubspaceName, PopulationRegistry, get_subspace
-from ..regression_models.hyperparameters import PlaceFieldHyperparameters
-
-clear_hyperparameters = False  # Clears hyperparameter cache
-clear_scores = False  # Clears score cache
-
-# This is for optimized hyperparameters on specific models
-score_subspaces = False  # Scores subspace models
-check_existing_scores = False  # Checks if scores already exist
-
-# This is for setting hyperparameters from a different model
-score_subspaces_from_hyps = False  # Scores subspace models from hyperparameters
-check_existing_scores_from_hyps = False  # Checks if scores already exist from hyperparameters
-source_model_name = "pca_subspace"
-source_method = "best"
+from dimensionality_manuscript.registry import SubspaceName, PopulationRegistry, get_subspace
+from dimensionality_manuscript.regression_models.hyperparameters import PlaceFieldHyperparameters
 
 # This is for setting specific hyperparameters
-clear_scores_from_hyps = False  # Clears score cache from specific hyperparameters
+clear_scores_from_hyps = True  # Clears score cache from specific hyperparameters
 score_with_specific_hyperparameters = False  # Scores subspace models with specific hyperparameters
-check_existing_with_specific_hyperparameters = True  # Checks if scores already exist with specific hyperparameters
+check_existing_with_specific_hyperparameters = False  # Checks if scores already exist with specific hyperparameters
 specific_hyperparameters = [
     PlaceFieldHyperparameters(num_bins=100, smooth_width=5.0),
     PlaceFieldHyperparameters(num_bins=100, smooth_width=None),
@@ -38,8 +25,7 @@ force_reoptimize = False  # Re-optimizes even if existing
 
 SUBSPACE_NAMES: list[SubspaceName] = [
     "pca_subspace",
-    # # # # "cvpca_subspace", # This is bad and doesn't make sense!!!
-    # # # # "svca_subspace", # This one also just sucks, even though it technically "makes sense".
+    "svca_subspace",  # This one also just sucks, even though it technically "makes sense".
     "covcov_subspace",
     "covcov_crossvalidated_subspace",
 ]
@@ -63,78 +49,9 @@ if __name__ == "__main__":
 
         for spks_type in SPKS_TYPES:
             for isession, session in enumerate(tqdm(sessiondb.iter_sessions(imaging=True, session_params=dict(spks_type=spks_type)))):
-                if clear_hyperparameters:
-                    subspace_model.clear_cached_hyperparameter(session, spks_type=spks_type, method=METHOD)
-
-                if clear_scores:
-                    subspace_model.clear_cached_score(session, spks_type=spks_type, method=METHOD)
-
                 if clear_scores_from_hyps:
                     for hyp in specific_hyperparameters:
                         subspace_model.clear_cached_score_from_hyps(session, spks_type=spks_type, hyperparameters=hyp)
-
-                if score_subspaces:
-                    try:
-                        _clear_cache = not subspace_model.check_existing_score(
-                            session,
-                            spks_type=spks_type,
-                            method=METHOD,
-                        )
-                        _ = subspace_model.get_best_score(
-                            session,
-                            spks_type=spks_type,
-                            force_remake=force_remake,
-                            force_reoptimize=force_reoptimize,
-                            method=METHOD,
-                        )
-
-                    except Exception as e:
-                        error_path = registry.registry_paths.subspace_error_path / f"{subspace_name}_{session.session_print(joinby='.')}.txt"
-                        error_path.parent.mkdir(parents=True, exist_ok=True)
-                        with open(error_path, "w") as f:
-                            f.write(str(e))
-                        print(f"Error scoring subspace {subspace_name} on session {session.session_print()}: {e}")
-                        continue
-
-                    finally:
-                        # Make sure any stored data is cleared (not actually sure if torch is saving things but better clear in case)
-                        if _clear_cache or force_remake:
-                            session.clear_cache()
-                            torch.cuda.empty_cache()
-                            gc.collect()
-
-                if score_subspaces_from_hyps:
-                    try:
-                        _clear_cache = not subspace_model.check_existing_score_from_hyps(
-                            session,
-                            spks_type=spks_type,
-                            source_model=get_subspace(source_model_name, registry),
-                            source_method=source_method,
-                            source_validation_split="validation",
-                        )
-                        _ = subspace_model.get_score(
-                            session,
-                            spks_type=spks_type,
-                            force_remake=force_remake,
-                            source_model=get_subspace(source_model_name, registry),
-                            source_method=source_method,
-                            source_validation_split="validation",
-                        )
-
-                    except Exception as e:
-                        error_path = registry.registry_paths.subspace_error_path / f"{subspace_name}_{session.session_print(joinby='.')}.txt"
-                        error_path.parent.mkdir(parents=True, exist_ok=True)
-                        with open(error_path, "w") as f:
-                            f.write(str(e))
-                        print(f"Error scoring subspace {subspace_name} on session {session.session_print()}: {e}")
-                        continue
-
-                    finally:
-                        # Make sure any stored data is cleared (not actually sure if torch is saving things but better clear in case)
-                        if _clear_cache or force_remake:
-                            session.clear_cache()
-                            torch.cuda.empty_cache()
-                            gc.collect()
 
                 if score_with_specific_hyperparameters:
                     for specific_hyperparameter in specific_hyperparameters:
@@ -165,28 +82,6 @@ if __name__ == "__main__":
                                 session.clear_cache()
                                 torch.cuda.empty_cache()
                                 gc.collect()
-
-                if check_existing_scores:
-                    if subspace_model.check_existing_score(session, spks_type=spks_type, method=METHOD):
-                        # print(f"{isession} Score for subspace {subspace_name} on session {session.session_print()} already exists")
-                        pass
-                    else:
-                        print(f"{isession} !!!!! Score for subspace {subspace_name} on session {session.session_print()} does not exist")
-                        pass
-
-                if check_existing_scores_from_hyps:
-                    if subspace_model.check_existing_score_from_hyps(
-                        session,
-                        spks_type=spks_type,
-                        source_model=get_subspace(source_model_name, registry),
-                        source_method=source_method,
-                        source_validation_split="validation",
-                    ):
-                        # print(f"{isession} Score for subspace {subspace_name} on session {session.session_print()} already exists")
-                        pass
-                    else:
-                        print(f"{isession} !!!!! Score for subspace {subspace_name} on session {session.session_print()} does not exist")
-                        pass
 
                 if check_existing_with_specific_hyperparameters:
                     for specific_hyperparameter in specific_hyperparameters:
