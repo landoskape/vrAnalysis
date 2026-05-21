f680d51b27edfda157231625ec0093ef431aac5b
Andrew Landau <andrewtylerlandau@gmail.com>
2026-05-21 09:03:48 +0100

    Update score_models.py

--- dimensionality_manuscript/workflows/score_models.py
+++ dimensionality_manuscript/workflows/score_models.py
@@ -65,55 +65,58 @@ SPKS_TYPES: tuple[SpksTypes] = (
 
 METHOD = "preferred"
 
+ACTIVITY_PARAMETERS_NAMES_TO_RUN: list[str] = ["default"]
+
 if __name__ == "__main__":
     sessiondb = get_database("vrSessions")
     registry = PopulationRegistry()
 
     for model_name in tqdm(MODEL_NAMES, desc="Testing different model types"):
-        model = get_model(model_name, registry)
+        for activity_parameters_name in ACTIVITY_PARAMETERS_NAMES_TO_RUN:
+            model = get_model(model_name, registry, activity_parameters=activity_parameters_name)
 
-        for spks_type in SPKS_TYPES:
-            for isession, session in enumerate(tqdm(sessiondb.iter_sessions(imaging=True, session_params=dict(spks_type=spks_type)))):
-                if clear_hyperparameters:
-                    model.clear_cached_hyperparameter(session, spks_type=spks_type, method=METHOD)
+            for spks_type in SPKS_TYPES:
+                for isession, session in enumerate(tqdm(sessiondb.iter_sessions(imaging=True, session_params=dict(spks_type=spks_type)))):
+                    if clear_hyperparameters:
+                        model.clear_cached_hyperparameter(session, spks_type=spks_type, method=METHOD)
 
-                if clear_scores:
-                    model.clear_cached_score(session, spks_type=spks_type, method=METHOD)
+                    if clear_scores:
+                        model.clear_cached_score(session, spks_type=spks_type, method=METHOD)
 
-                if score_models:
-                    _clear_cache = False
-                    try:
-                        _clear_cache = not model.check_existing_score(
-                            session,
-                            spks_type=spks_type,
-                            method=METHOD,
-                        )
-                        _ = model.get_best_score(
-                            session,
-                            spks_type=spks_type,
-                            force_remake=force_remake,
-                            force_reoptimize=force_reoptimize,
-                            method=METHOD,
-                        )
+                    if score_models:
+                        _clear_cache = False
+                        try:
+                            _clear_cache = not model.check_existing_score(
+                                session,
+                                spks_type=spks_type,
+                                method=METHOD,
+                            )
+                            _ = model.get_best_score(
+                                session,
+                                spks_type=spks_type,
+                                force_remake=force_remake,
+                                force_reoptimize=force_reoptimize,
+                                method=METHOD,
+                            )
 
-                    except Exception as e:
-                        error_path = registry.registry_paths.error_path / f"{model_name}_{session.session_print(joinby='.')}.txt"
-                        with open(error_path, "w") as f:
-                            f.write(str(e))
-                        print(f"Error scoring model {model_name} on session {session.session_print()}: {e}")
-                        continue
+                        except Exception as e:
+                            error_path = registry.registry_paths.error_path / f"{model_name}_{session.session_print(joinby='.')}.txt"
+                            with open(error_path, "w") as f:
+                                f.write(str(e))
+                            print(f"Error scoring model {model_name} on session {session.session_print()}: {e}")
+                            continue
 
-                    finally:
-                        if _clear_cache:
-                            session.clear_cache()
-                            registry.clear_population_cache()
-                            torch.cuda.empty_cache()
-                            gc.collect()
+                        finally:
+                            if _clear_cache:
+                                session.clear_cache()
+                                registry.clear_population_cache()
+                                torch.cuda.empty_cache()
+                                gc.collect()
 
-                if check_existing_scores:
-                    if model.check_existing_score(session, spks_type=spks_type, method=METHOD):
-                        # print(f"{isession} Score for model {model_name} on session {session.session_print()} already exists")
-                        pass
-                    else:
-                        print(f"{isession} !!!!! Score for model {model_name} on session {session.session_print()} does not exist")
-                        pass
+                    if check_existing_scores:
+                        if model.check_existing_score(session, spks_type=spks_type, method=METHOD):
+                            # print(f"{isession} Score for model {model_name} on session {session.session_print()} already exists")
+                            pass
+                        else:
+                            print(f"{isession} !!!!! Score for model {model_name} on session {session.session_print()} does not exist")
+                            pass
